6f10871b0d512957bb82060087858873fbb80bce
Yu Yang <yutingyang@yus-macbook-pro.local>
2018-09-30 13:39:32 -0400

    Initial Commit

---
+++ Assignment1_FlixTests/Assignment1_FlixTests.swift
@@ -0,0 +1,36 @@
+//
+//  Assignment1_FlixTests.swift
+//  Assignment1_FlixTests
+//
+//  Created by Yu Ting Yang on 9/30/18.
+//  Copyright © 2018 Yu Ting Yang. All rights reserved.
+//
+
+import XCTest
+@testable import Assignment1_Flix
+
+class Assignment1_FlixTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
