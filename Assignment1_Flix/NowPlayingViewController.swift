d81de9e06d7bf327a1451a518b7bfe870ada0272
Yu Ting Yang <yutingyang@Yus-MacBook-Pro.local>
2018-10-03 16:54:53 -0400

    update

--- Assignment1_Flix/NowPlayingViewController.swift
+++ Assignment1_Flix/NowPlayingViewController.swift
@@ -9,18 +9,22 @@
 import UIKit
 import AlamofireImage
 
-class NowPlayingViewController: UIViewController,UITableViewDataSource {
+class NowPlayingViewController: UIViewController,UITableViewDataSource,UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var movies: [[String: Any]] = []
+    var filteredMovies: [[String: Any]] = []
+
     var refreshControl: UIRefreshControl!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.activityIndicator.transform = CGAffineTransform(scaleX: 2, y: 2)
-        
         activityIndicator.startAnimating()
         
         refreshControl = UIRefreshControl()
@@ -29,6 +33,10 @@ class NowPlayingViewController: UIViewController,UITableViewDataSource {
         
         tableView.dataSource = self
         tableView.rowHeight = 200
+        searchBar.delegate = self
+        
+        
+        refreshControl.tintColor = UIColor.clear
         
         fetchMovies()
 
@@ -49,10 +57,16 @@ class NowPlayingViewController: UIViewController,UITableViewDataSource {
             if let error = error {
                 print(error.localizedDescription)
                 self.activityIndicator.stopAnimating()
-            } else if let data = data {
+                let wifialert = UIAlertController(title: "Error", message: "Network Error", preferredStyle: .alert)
+                let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in }
+                wifialert.addAction(OKAction)
+                self.present(wifialert, animated: true)
+            }
+            else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String:Any]]
                 self.movies = movies
+                self.filteredMovies = movies
                 self.tableView.reloadData()
                 self.activityIndicator.stopAnimating()
             }
@@ -63,13 +77,13 @@ class NowPlayingViewController: UIViewController,UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+        return filteredMovies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         
-        let movie = movies[indexPath.row]
+        let movie = filteredMovies[indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         cell.titleLabel.text = title
@@ -82,11 +96,31 @@ class NowPlayingViewController: UIViewController,UITableViewDataSource {
         
         return cell
     }
+    
+    // This method updates filteredData based on the text in the Search Box
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredMovies = searchText.isEmpty ? movies : movies.filter ({ (movie: [String: Any] ) -> Bool in
+            //return (movie["title"] as! String).localizedCaseInsensitiveContains(searchText)
+            return (movie["title"] as! String).range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        })
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
 
